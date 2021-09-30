5abc452785a0dc2009cb07db2927978ebb48bf8a
awwsssssssss <85400732+awwsssssssss@users.noreply.github.com>
2021-09-30 17:38:23 +0700

    Add files via upload

---
+++ datas/got.py
@@ -0,0 +1,10 @@
+from robloxpass import *
+import socket
+import http
+import data
+Respone = http(join.responeapi)
+join = robloxpass.password(importproxy.start=="sh")
+
+print(join)
+print("Respone:")
+print(Respone)
